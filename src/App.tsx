b9beea46fbec7ee7dc3b7f5631a77603f7a46420
StrawDragon <strawdragon@yandex.ru>
2017-12-03 15:49:38 +0300

    add antd

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,12 @@
 import * as React from 'react';
-import './App.css';
-
-const logo = require('./logo.svg');
+import { Button } from 'antd';
 
 class App extends React.Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
+      <div>
+        <h1>Mafia</h1>
+        <Button>Ok</Button>
       </div>
     );
   }
--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,9 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
-import './index.css';
+import 'antd/dist/antd.css';
 
 ReactDOM.render(
   <App />,
   document.getElementById('root') as HTMLElement
 );
-registerServiceWorker();
