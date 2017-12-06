84754f3d5d9221fdd87be05bead3655fe6dc1a62
StrawDragon <strawdragon@yandex.ru>
2017-12-06 22:06:26 +0300

    add grid in player_list

--- src/game_card/index.tsx
+++ src/game_card/index.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { Row } from 'antd';
 import PlayerList from './player_list';
 
 const players = [
@@ -15,15 +16,75 @@ const players = [
     nickname: 'Бордерлендс',
     isDead: false,
     warningCount: 0,
-  }
+  },
+  {
+    id: '67',
+    avatar: 'https://images.stopgame.ru/uploads/avatars/c64x64/-OD2ShYwAG2ELpwGBv7w7A/avatar_422038.jpg',
+    nickname: 'Бордерлендс',
+    isDead: false,
+    warningCount: 0,
+  },
+  {
+    id: '69',
+    avatar: 'https://images.stopgame.ru/uploads/avatars/c64x64/-OD2ShYwAG2ELpwGBv7w7A/avatar_422038.jpg',
+    nickname: 'Бордерлендс',
+    isDead: false,
+    warningCount: 0,
+  },
+  {
+    id: '12',
+    avatar: 'https://images.stopgame.ru/uploads/avatars/c64x64/-OD2ShYwAG2ELpwGBv7w7A/avatar_422038.jpg',
+    nickname: 'Бордерлендс',
+    isDead: false,
+    warningCount: 0,
+  },
+  {
+    id: '34',
+    avatar: 'https://images.stopgame.ru/uploads/avatars/c64x64/-OD2ShYwAG2ELpwGBv7w7A/avatar_422038.jpg',
+    nickname: 'Бордерлендс',
+    isDead: false,
+    warningCount: 0,
+  },
+  {
+    id: '45',
+    avatar: 'https://images.stopgame.ru/uploads/avatars/c64x64/-OD2ShYwAG2ELpwGBv7w7A/avatar_422038.jpg',
+    nickname: 'Бордерлендс',
+    isDead: false,
+    warningCount: 0,
+  },
+  {
+    id: '56',
+    avatar: 'https://images.stopgame.ru/uploads/avatars/c64x64/-OD2ShYwAG2ELpwGBv7w7A/avatar_422038.jpg',
+    nickname: 'Бордерлендс',
+    isDead: false,
+    warningCount: 0,
+  },
+  {
+    id: '523',
+    avatar: 'https://images.stopgame.ru/uploads/avatars/c64x64/cPWIqei9T4-17K5J-UMpnQ/avatar_284274.jpg',
+    nickname: 'Джокер',
+    isDead: false,
+    warningCount: 0,
+  },
+  {
+    id: '98',
+    avatar: 'https://images.stopgame.ru/uploads/avatars/c64x64/cPWIqei9T4-17K5J-UMpnQ/avatar_284274.jpg',
+    nickname: 'Джокер',
+    isDead: false,
+    warningCount: 0,
+  },
 ];
 
 class GameCard extends React.Component {
   render() {
     return (
       <div className="align-center">
-        <h1>Карточка игры</h1>
-        <div>Управление процесом, этап игры, таймер - (Managment)</div>
+        <Row>
+          <h1>Карточка игры</h1>
+        </Row>
+        <Row>
+            <h4>Управление процесом, этап игры, таймер - (Managment)</h4>
+        </Row>
         <PlayerList players={players} />
         <div>Голосование</div>
         <div>Темные делишки</div>
--- src/game_card/player_list/index.tsx
+++ src/game_card/player_list/index.tsx
@@ -1,30 +1,48 @@
 import * as React from 'react';
+import { Row, Col } from 'antd';
 import Player from '../types/player';
 import PlayerCard from '../player_card';
+
+import './style.css';
 
 interface Props {
   players: Array<Player>;
 }
 
 class PlayerList extends React.Component<Props> {
-  renderPlayerCards(players: Array<Player>) {
-    return players.map((player, index) => (
+  renderPlayerCard(index: number, player: Player) {
+    return (
       <PlayerCard
         key={index}
         value={player}
         number={index + 1}
       />
+    );
+  }
+  renderPlayerCards(players: Array<Player>, indexBase: number) {
+    return players.map((player, index) => (
+      <Col lg={4} xl={2}>
+        {this.renderPlayerCard(index + indexBase, player)}
+      </Col>
     ));
   }
 
   render() {
     const { players } = this.props;
-    const playerCards = this.renderPlayerCards(players);
 
     return (
       <div>
+        <h4>
           Список игроков 2
-          {playerCards}
+        </h4>
+        <Row>
+          <Col lg={2} xl={2}></Col>
+          {this.renderPlayerCards(players.slice(0, 5), 0)}
+          <Col lg={2} xl={0}></Col>
+          <Col lg={2} xl={0}></Col>
+          {this.renderPlayerCards(players.slice(5), 5)}
+          <Col lg={2} xl={2}></Col>
+        </Row>
       </div>
     );
   }
