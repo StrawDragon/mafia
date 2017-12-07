fc4824ca82fbbc0067c4450d60871acf8a2d30e9
LonelyGriffin <lonelygriffin@gmail.com>
2017-12-07 12:54:28 +0300

    add props interface

--- src/game_card/voting/index.tsx
+++ src/game_card/voting/index.tsx
@@ -4,8 +4,46 @@ import { Tabs, Card, Avatar } from 'antd';
 const TabPane = Tabs.TabPane;
 const { Meta } = Card as any;
 
+interface VotingOfDay {
+  dayNumber: number;
+  players: Array<{
+    id: string;
+    voted: boolean;
+    votes: Array<string>; // id of player votes
+  }>;
+  carCrashes: Array<{
+    step1: Array<{
+        playerID: string;
+        voted: boolean;
+        votes: Array<string>; // id of player votes
+    }>,
+    step2: Array<{
+      playerIDs: Array<string>,
+      voted: boolean;
+      votes: Array<string>; // id of player votes
+    }>,
+  }>;
+}
+
+interface Props {
+  value: Array<VotingOfDay>;
+  players: Array<{
+    id: string;
+    playerNumber: number;
+    avatar: string;
+    nickname: string; 
+  }>;
+}
+
+const props: Props = {
+  value: [],
+  players: [],
+};
+
 class Voting extends React.Component {
   render () {
+    const { players } = props;
+
     return (
       <Tabs defaultActiveKey="5" size="small" tabPosition="left">
         <TabPane tab="День 5" key="5">
