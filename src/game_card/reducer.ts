50f6243cd36c505e909b8ff5aa4aa1786837ce94
LonelyGriffin <lonelygriffin@gmail.com>
2017-12-10 23:18:06 +0300

    add stub

--- src/game_card/reducer.ts
+++ src/game_card/reducer.ts
@@ -1,12 +1,31 @@
 import { Action } from '../common/types/action';
+import Vote from './types/vote';
+import Voting from './types/voting';
+import Player from './types/player';
+import SheriffCheck from './types/sheriff_check';
+import Shoot from './types/shoot';
+
+import initialStateSub from './state_stub';
 
 export interface GameCardState {
-
+  stage: number;
+  votes: Array<Vote>;
+  votings: Array<Voting>;
+  players: Array<Player>;
+  sheriffChecks: Array<SheriffCheck>;
+  shoots: Array<Shoot>;
 }
 
-const initialState: GameCardState = {
+// const initialState: GameCardState = {
+//   stage: 0,
+//   votes: [],
+//   votings: [],
+//   players: [],
+//   sheriffChecks: [],
+//   shoots: [],
+// };
 
-};
+const initialState = initialStateSub;
 
 const reducer = (state: GameCardState = initialState, action: Action<{}>) => {
   return state;
