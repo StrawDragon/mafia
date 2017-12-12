dfc3a9f71c24b297335a07291ca40d848ca9484b
LonelyGriffin <lonelygriffin@gmail.com>
2017-12-12 11:26:19 +0300

    add day voting component

--- src/game_card/voting/day_voting.tsx
+++ src/game_card/voting/day_voting.tsx
@@ -1,7 +1,62 @@
 import * as React from 'react';
+import { connect } from 'react-redux';
+import { RootState } from '../../common/reducer/root';
+import Voting from '../types/voting';
+import Player from '../types/player';
+import VotingForPlayer from './voting_for_player';
+import selectNormalDayVotings from '../selectors/select_normal_day_votings';
 
 interface Props {
-  
+  votings: Array<Voting>;
+  players: Array<Player>;
 }
 
-const DayVoting
+const compareByOrder = (v1: Voting, v2: Voting) => v1.order - v2.order;
+const sortVotingsByOrder = (votings: Array<Voting>) => votings.sort(compareByOrder);
+
+class DayVotingComponent extends React.Component<Props> {
+  renderVoting = (voting: Voting, player: Player) => {
+    return (
+      <div>
+        <span>
+          {player.nickname}
+        </span>
+        <VotingForPlayer voting={voting}/>
+      </div>
+    );
+  }
+  renderVotings = (votings: Array<Voting>, players: Array<Player>) => {
+    return sortVotingsByOrder(votings).map((voting) => {
+      const votingPlayer = players.find(player => player.id === voting.playerID);
+      return votingPlayer
+        ? this.renderVoting(voting, votingPlayer)
+        : false;
+    });
+  }
+
+  render() {
+    const { votings, players } = this.props;
+    
+    return (
+      <div>
+        <span>
+          Голосование
+        </span>
+        {this.renderVotings(votings, players)}
+      </div>
+    );
+  }
+}
+
+interface OuterProps {
+  dayNumber: number;
+}
+
+const DayVoting = connect(
+  (state: RootState, outerProps: OuterProps) => ({
+    players: state.gameCard.players,
+    votings: selectNormalDayVotings(state, outerProps.dayNumber),
+  })
+)(DayVotingComponent);
+
+export default DayVoting;
