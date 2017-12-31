166c4b1e8d8737ec68c6a6522ff27e8fa84e7730
LonelyGriffin <lonelygriffin@gmail.com>
2017-12-31 17:28:52 +0300

    [game-management] add screen changing

--- src/game_card/index.tsx
+++ src/game_card/index.tsx
@@ -5,8 +5,25 @@ import { LayoutContent } from '../common/components/layout/content';
 import { LayoutBody } from '../common/components/layout/body';
 import { LayoutHeader } from '../common/components/layout/header';
 import { CardDistribution } from './card_distribution';
+import { connect } from 'react-redux';
+import { RootState } from '../common/reducer/root';
+import StageType from './types/stage_type';
 
-class GameCard extends React.Component {
+interface Props {
+  stageType: StageType;
+}
+
+class GameCard extends React.Component<Props> {
+  renderContent = () => {
+    const { stageType } = this.props;
+
+    switch (stageType) {
+      case StageType.CARD_DISTRIBUTION: return <CardDistribution />;
+      case StageType.MAFIA_COLLUSION: return <span>MAFIA_COLLUSION</span>;
+      default: return false;
+    }
+  }
+
   render() {
     return (
       <LayoutRoot>
@@ -15,7 +32,7 @@ class GameCard extends React.Component {
         </LayoutHeader>
         <LayoutBody>
           <LayoutContent>
-            <CardDistribution />
+            {this.renderContent()}
           </LayoutContent>
         </LayoutBody>
       </LayoutRoot>
@@ -23,4 +40,8 @@ class GameCard extends React.Component {
   }
 }
 
-export default GameCard;
+export default connect(
+  (state: RootState) => ({
+    stageType: state.gameCard.stage.type
+  })
+)(GameCard);
