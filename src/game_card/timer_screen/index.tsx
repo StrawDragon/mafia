77fd66ac2b6081af6e29b5329eab6207898dec69
StrawDragon <strawdragon@yandex.ru>
2018-01-06 18:05:49 +0300

    add TimerScreen for simple stage

---
+++ src/game_card/timer_screen/index.tsx
@@ -0,0 +1,35 @@
+import * as React from 'react';
+import { connect } from 'react-redux';
+import * as Action from '../actions';
+import { PlayerList } from '../player_list';
+import { Fragment } from '../../common/components/fragment';
+import { GameManagement } from '../game_management';
+import Timer from '../timer';
+
+interface Props {
+  stageTitle: string;
+  nextDescription: string;
+  onNext: () => void;
+}
+
+class TimerScreenComponent extends React.Component<Props> {
+  render() {
+    const { onNext, stageTitle, nextDescription } = this.props;
+    return (<Fragment>
+      <GameManagement
+        title={stageTitle}
+        nextDescription={nextDescription}
+        onNext={onNext}
+      />
+      <Timer />
+      <PlayerList />
+    </Fragment>);
+  }
+}
+
+export const TimerScreen = connect(
+  () => ({}),
+  (dispatch) => ({
+    onNext: () => { dispatch(Action.requestNext()) }
+  }),
+)(TimerScreenComponent);
