9c2ea8e60e3a1c83d4d4d066dbdf9f1600c46943
LonelyGriffin <lonelygriffin@gmail.com>
2018-01-22 10:32:24 +0300

    remove deprecated test

--- src/test.test.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from 'react';
-import { shallow } from 'enzyme';
-
-describe('test work', () => {
-  it('yep work', () => {
-    const wrapper = shallow(<div/>);
-    expect(wrapper.exists()).toBe(true);
-  });
-});
