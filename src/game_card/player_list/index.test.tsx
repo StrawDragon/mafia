ff8088a2bed5c153c9687c30578ce86805469fd7
LonelyGriffin <lonelygriffin@gmail.com>
2018-01-23 09:54:21 +0300

    add more logic to player list

--- src/game_card/player_list/index.test.tsx
+++ src/game_card/player_list/index.test.tsx
@@ -1,7 +1,6 @@
 import * as React from 'react';
 import { shallow } from 'enzyme';
 import { PlayerListComponent, Props } from './index';
-import { CardSize } from '../types/card_size';
 import Player from '../types/player';
 import { PlayerCard } from '../player_card';
 import PlayerRole from '../types/player_role';
@@ -16,9 +15,21 @@ const getPlayersByNames = (names: string[]): Player[] =>
     role: PlayerRole.Citizen,
   }));
 
+const getPlayersByDayDeathNumbers = (dayDeathNumbers: Array<number | undefined>): Player[] =>
+  dayDeathNumbers.map((dayDeathNumber, index) => ({
+    id: `id ${index}`,
+    numberAtTable: index,
+    nickname: `name ${index}`,
+    avatar: 'src',
+    dayDeathNumber,
+    warningCount: 0,
+    role: PlayerRole.Citizen,
+  }));
+
 const render = (overwriteProps: Partial<Props> = {}) => {
   const props = {
     players: [],
+    currentDay: 0,
     ...overwriteProps,
   };
 
@@ -26,7 +37,7 @@ const render = (overwriteProps: Partial<Props> = {}) => {
 
   return {
     wrapper,
-    playersWrapper: () => wrapper.find(PlayerCard),
+    playerCardWrappers: () => wrapper.find(PlayerCard),
   };
 };
 
@@ -44,16 +55,94 @@ describe('game_card/player_list', () => {
       
       const players = getPlayersByNames([name0, name1, name2]);
 
-      const { playersWrapper } = render({ players });
+      const { playerCardWrappers } = render({ players });
 
-      expect(playersWrapper().length).toBe(3);
-      expect(playersWrapper().at(0).props().value.nickname).toBe(name0);
-      expect(playersWrapper().at(1).props().value.nickname).toBe(name1);
-      expect(playersWrapper().at(2).props().value.nickname).toBe(name2);
+      expect(playerCardWrappers().length).toBe(3);
+      expect(playerCardWrappers().at(0).props().value.nickname).toBe(name0);
+      expect(playerCardWrappers().at(1).props().value.nickname).toBe(name1);
+      expect(playerCardWrappers().at(2).props().value.nickname).toBe(name2);
     });
-    // должен предоставлять метод для отрисовки дополнительной информации в карточке
-    // должен поддерживать разные размеры карточек
-    // выделение игрока
+    it('должен предоставлять метод для отрисовки дополнительной информации в карточке', () => {
+      const name0 = 'name 0';
+      const name1 = 'name 1';
+      const name2 = 'name 2';
+      
+      const players = getPlayersByNames([name0, name1, name2]);
+      const cardContentRenderer = jest.fn((value: Player) => value.nickname);
+
+      const { playerCardWrappers } = render({ players, cardContentRenderer });
+
+      expect(cardContentRenderer.mock.calls.length).toBe(3);
+
+      expect(cardContentRenderer.mock.calls[0][0]).toBe(players[0]);
+      expect(cardContentRenderer.mock.calls[1][0]).toBe(players[1]);
+      expect(cardContentRenderer.mock.calls[2][0]).toBe(players[2]);
+
+      expect(playerCardWrappers().at(0).contains(name0)).toBe(true);
+      expect(playerCardWrappers().at(1).contains(name1)).toBe(true);
+      expect(playerCardWrappers().at(2).contains(name2)).toBe(true);
+    });
+    it('должен по умолчанию устанавливать размер карточек в normal', () => {
+      const name0 = 'name 0';
+      const name1 = 'name 1';
+      const name2 = 'name 2';
+      
+      const players = getPlayersByNames([name0, name1, name2]);
+
+      const { playerCardWrappers } = render({ players });
+
+      expect(playerCardWrappers().at(0).props().size).toBe('normal');
+      expect(playerCardWrappers().at(1).props().size).toBe('normal');
+      expect(playerCardWrappers().at(2).props().size).toBe('normal');
+    });
+    it('должен позволять устанавливать размер карточек в small', () => {
+      const name0 = 'name 0';
+      const name1 = 'name 1';
+      const name2 = 'name 2';
+      
+      const cardSize = 'small';
+      const players = getPlayersByNames([name0, name1, name2]);
+
+      const { playerCardWrappers } = render({ players, cardSize });
+
+      expect(playerCardWrappers().at(0).props().size).toBe(cardSize);
+      expect(playerCardWrappers().at(1).props().size).toBe(cardSize);
+      expect(playerCardWrappers().at(2).props().size).toBe(cardSize);
+    });
+    it('должен позволять выделить игрока по номеру за столом', () => {
+      const name0 = 'name 0';
+      const name1 = 'name 1';
+      const name2 = 'name 2';
+      
+      const selectedPlayerNumber = 1;
+      const players = getPlayersByNames([name0, name1, name2]);
+
+      const { playerCardWrappers } = render({ players, selectedPlayerNumber });
+
+      expect(playerCardWrappers().at(0).props().selected).toBe(false);
+      expect(playerCardWrappers().at(1).props().selected).toBe(true);
+      expect(playerCardWrappers().at(2).props().selected).toBe(false);
+    });
+    it('по умолчанию должен дисайблить мертвых', () => {
+      const currentDay = 1;
+      const players = getPlayersByDayDeathNumbers([undefined, 3, 0]);
+
+      const { playerCardWrappers } = render({ players, currentDay });
+
+      expect(playerCardWrappers().at(0).props().disabled).toBe(false);
+      expect(playerCardWrappers().at(1).props().disabled).toBe(false);
+      expect(playerCardWrappers().at(2).props().disabled).toBe(true);
+    });
+    it('должен предоставлять опцию для отключения дисейблинга мертвых', () => {
+      const currentDay = 1;
+      const players = getPlayersByDayDeathNumbers([undefined, 3, 0]);
+
+      const { playerCardWrappers } = render({ players, currentDay, disableDeathPlayers: false });
+
+      expect(playerCardWrappers().at(0).props().disabled).toBe(false);
+      expect(playerCardWrappers().at(1).props().disabled).toBe(false);
+      expect(playerCardWrappers().at(2).props().disabled).toBe(false);
+    });
     // опциональный дизейблинг мертвых игроков
   });
 });
--- src/game_card/player_list/index.tsx
+++ src/game_card/player_list/index.tsx
@@ -7,28 +7,43 @@ import { CardSize } from '../types/card_size';
 import styles from './style.css';
 
 export interface Props {
-  cardSize?: CardSize;
   players: Array<Player>;
+  currentDay: number;
+  cardSize?: CardSize;
+  selectedPlayerNumber?: number;
   cardContentRenderer?: (player: Player) => React.ReactNode;
+  disableDeathPlayers?: boolean;
 }
 
 export class PlayerListComponent extends React.Component<Props> {
   renderPlayerCards(players: Array<Player>) {
-    return players.map((player, index) => (
-      <li className={styles.item} key={index}>
-        <PlayerCard
-          value={player}
-          size={this.props.cardSize}
-          disabled={player.dayDeathNumber !== undefined}
-          selected={player.numberAtTable === 3}
-        >
-          {this.props.cardContentRenderer ?
-            this.props.cardContentRenderer(player) :
-            null
-          }
-        </PlayerCard>
-      </li>
-    ));
+    return players.map((player, index) => {
+      const { disableDeathPlayers, selectedPlayerNumber, currentDay, cardSize } = this.props;
+      const { dayDeathNumber, numberAtTable } = player;
+
+      const disabled = disableDeathPlayers || disableDeathPlayers === undefined
+        ? dayDeathNumber !== undefined && dayDeathNumber <= currentDay
+        : false;
+
+      const selected = numberAtTable === selectedPlayerNumber;
+      const size = cardSize || 'normal';
+
+      return (
+        <li className={styles.item} key={index}>
+          <PlayerCard
+            value={player}
+            size={size}
+            disabled={disabled}
+            selected={selected}
+          >
+            {this.props.cardContentRenderer ?
+              this.props.cardContentRenderer(player) :
+              null
+            }
+          </PlayerCard>
+        </li>
+      );
+    });
   }
 
   render() {
@@ -43,7 +58,8 @@ export class PlayerListComponent extends React.Component<Props> {
 }
 
 const mapStateToProps = (state: RootState) => ({
-  players: state.gameCard.players
+  players: state.gameCard.players,
+  currentDay: state.gameCard.day,
 });
 
 export const PlayerList = connect(
