53c6e123ab5461502bc3c7c14644553c3e60329a
LonelyGriffin <lonelygriffin@gmail.com>
2018-02-04 10:18:19 +0300

    refactoring voting screen

--- src/game_card/voting_screen/index.test.tsx
+++ src/game_card/voting_screen/index.test.tsx
@@ -1,22 +1,12 @@
 import * as React from 'react';
 import { VotingScreenComponent, Props } from './index';
 import { mount } from 'enzyme';
-import Player from '../types/player';
+import { makePlayer, makePlayersByIDs } from '../utils/player';
 import Vote from '../types/vote';
-import PlayerRole from '../types/player_role';
+
 import { GameManagement } from '../game_management';
 import { PlayerListComponent } from '../player_list';
 import { PlayerContent } from './player_content';
-
-const makePlayer = (override: Partial<Player> = {}): Player => ({
-  id: override.id || 'id',
-  numberAtTable: override.numberAtTable || 0,
-  nickname: override.nickname || 'name',
-  avatar: override.avatar || 'src',
-  dayDeathNumber: override.dayDeathNumber || undefined,
-  warningCount: override.warningCount || 0,
-  role: override.role ||  PlayerRole.Citizen,
-});
 
 const makeVote = (override: Partial<Vote> = {}): Vote => ({
   id: 'id',
@@ -27,8 +17,6 @@ const makeVote = (override: Partial<Vote> = {}): Vote => ({
 
 const makeVotesByPlayerIDs = (IDs: string[]) => IDs.map(id => makeVote({ playerID: id }));
 
-const makePlayersByIDs = (IDs: string[]) => IDs.map(id => makePlayer({ id }));
-
 const render = (overrideProps: Partial<Props> = {}) => {
   const props = {
     currentDay: 0,
@@ -36,12 +24,12 @@ const render = (overrideProps: Partial<Props> = {}) => {
     votes: [],
     onToggleVote: () => ({}),
     onNext: () => ({}),
-    elimenationPlayer: makePlayer({ nickname: 'elimenationPlayer' }),
+    eliminationPlayer: makePlayer({ nickname: 'eliminationPlayer' }),
     ...overrideProps
   };
-  
+
   const wrapper = mount(<VotingScreenComponent {...props} />);
-  
+
   return {
     wrapper,
     gameManagementWrapper: () => wrapper.find(GameManagement),
@@ -59,14 +47,14 @@ describe('game_card / voting_screen', () => {
     });
     it('Должен отображать в заголовке сообщение об голосовании за игрока', () => {
       const playerName = 'test name';
-      const elimenationPlayer = makePlayer({ nickname: playerName });
+      const eliminationPlayer = makePlayer({ nickname: playerName });
 
       const { gameManagementWrapper } = render({ eliminationPlayer });
 
       expect(gameManagementWrapper().at(0).props().title).toBe(`Голосование за вылет игрока ${playerName}`);
     });
     it('Должен вызывать onNext при нажатии на соотв кнопку', () => {
-      const elimenationPlayer = makePlayer({ nickname: 'test name' });
+      const eliminationPlayer = makePlayer({ nickname: 'test name' });
       const onNext = jest.fn();
       const { gameManagementWrapper } = render({ eliminationPlayer, onNext });
 
@@ -88,7 +76,7 @@ describe('game_card / voting_screen', () => {
       expect(playerContents().at(2).props().value).toBe(true);
     });
     it('Должен при нажатии кнопки голоса вызывать onToggle и передавать id игроков который голосует и за которого голосуют', () => {
-      const elimenationPlayer = makePlayer({ id: 'elimenationPlayer id' });
+      const eliminationPlayer = makePlayer({ id: 'eliminationPlayer id' });
       const player1 = makePlayer({ id: 'player1 id' });
       const player2 = makePlayer({ id: 'player2 id' });
       const onToggleVote = jest.fn();
@@ -99,9 +87,9 @@ describe('game_card / voting_screen', () => {
       playerContents().at(0).props().onToggleVote();
       playerContents().at(1).props().onToggleVote();
 
-      expect(onToggleVote.mock.calls[0][0]).toBe(elimenationPlayer.id);
+      expect(onToggleVote.mock.calls[0][0]).toBe(eliminationPlayer.id);
       expect(onToggleVote.mock.calls[0][1]).toBe(player1.id);
-      expect(onToggleVote.mock.calls[0][0]).toBe(elimenationPlayer.id);
+      expect(onToggleVote.mock.calls[0][0]).toBe(eliminationPlayer.id);
       expect(onToggleVote.mock.calls[1][1]).toBe(player2.id);
     });
     it('Должен давать возможность дисейблить список игроков', () => {
@@ -109,7 +97,6 @@ describe('game_card / voting_screen', () => {
 
       expect(playerListWrapper().props().disable).toBe(true);
     });
-    // Подумать над nextDescription
 
     // Вынести makePlayer в utils, сделать рефакторинг для других тестов
   });
